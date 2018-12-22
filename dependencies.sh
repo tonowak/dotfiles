c8b432a7fd7fe38a810378159ba9614288938f19
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2018-12-22 17:54:38 +0100

    Dependency script

---
+++ dependencies.sh
@@ -0,0 +1,6 @@
+# needed:
+# trizen git
+trizen -S compton-tryone-git i3-gaps polybar
+trizen -S i3lock-fancy-git neofetch rofi
+trizen -S termite zsh vim
+trizen -S ttf-dejavu ttf-font-awesome ttf-ubuntu-font-family powerline-fonts
