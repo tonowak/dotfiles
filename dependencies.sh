9288ff091d62095d6a7af205b4ba0146ceb2db74
Tomasz Nowak <tomek.nowak.gm.16@gmail.com>
2018-12-23 17:33:00 +0100

    Adding GTK applications

--- dependencies.sh
+++ dependencies.sh
@@ -4,3 +4,7 @@ trizen -S compton-tryone-git i3-gaps polybar
 trizen -S i3lock-fancy-git neofetch rofi feh
 trizen -S termite zsh vim
 trizen -S ttf-dejavu ttf-font-awesome ttf-ubuntu-font-family powerline-fonts
+trizen -S redshift spotify
+trizen -S lxappearance equilux-theme papirus-icon-theme-git
+trizen -S transmission-gtk gnome-disk-utility nautilus file-roller eog gnome-screenshot gucharmap vlc usb-creator-gtk
+trizen -S 7z wget bc clang xclip
